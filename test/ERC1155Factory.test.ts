39d7d5d925b9dccd1c2d8c1fbb3a15a73dc03014
ahsenriaz <ahsenriaz2001@gmail.com>
2023-01-15 22:58:16 +0500

    Update ERC1155Factory.test.ts

--- test/ERC1155Factory.test.ts
+++ test/ERC1155Factory.test.ts
@@ -1,8 +1,7 @@
 import { expect, assert } from "chai";
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-
-import { ERC1155Factory__factory, ERC1155Factory } from "../typechain-types";
+import { ERC1155Factory, ERC1155Factory__factory } from "../typechain-types";
 
 describe("ERC1155Factory Contract", () => {
   let erc1155Factory: ERC1155Factory;
@@ -27,10 +26,93 @@ describe("ERC1155Factory Contract", () => {
     });
   });
 
-  describe("Initialization", () => {
+  describe("Factory Initialization", () => {
     it("Should initialize ERC1155 Factory Contract", async () => {
-        const name = "DiamondNXT SFT Factory";
-        await erc1155Factory.initialize(name, );
+      const name = "DiamondNXT ERC1155 Factory";
+      const symbol = "DNXT";
+      await erc1155Factory.connect(owner).initialize(name, symbol);
+      expect(await erc1155Factory.name()).to.equal(name);
+      expect(await erc1155Factory.symbol()).to.equal(symbol);
+    });
+  });
+
+  describe("Create ERC1155 Collection", () => {
+    it("should create an erc1155 collection", async () => {
+      const name = "RoboCop";
+      const symbol = "RC";
+
+      const tx = await erc1155Factory
+        .connect(addr1)
+        .createCollection(name, symbol, addr1.address);
+      const res = await tx.wait();
+
+      if (res.events && res.events[1] && res.events[1].args) {
+        expect(res.events && res.events[1].event).to.equal(
+          "ERC1155CollectionCreated"
+        );
+
+        const collectionAddress = res.events && res.events[1].args.collection;
+
+        const collection = await ethers.getContractAt(
+          "ERC1155Asset",
+          collectionAddress
+        );
+
+        expect(await erc1155Factory.createdCollections(collectionAddress)).to.be
+          .true;
+
+        expect(await collection.name()).to.equal(name);
+        expect(await collection.symbol()).to.equal(symbol);
+        expect(await collection.totalMinted()).to.equal(0);
+      }
+    });
+
+    it("Should create an nft collection and mint an nft", async () => {
+      const name = "RoboCop";
+      const symbol = "RC";
+
+      const token = {
+        to: addr1.address,
+        tokenId: 10,
+        amount: 5,
+        metadataUri: `ipfs://token-22`,
+      };
+
+      const tx = await erc1155Factory
+        .connect(addr1)
+        .createCollection(name, symbol, addr1.address);
+      const res = await tx.wait();
+
+      if (res.events && res.events[1] && res.events[1].args) {
+        expect(res.events && res.events[1].event).to.equal(
+          "ERC1155CollectionCreated"
+        );
+
+        const collectionAddress = res.events && res.events[1].args.collection;
+
+        expect(await erc1155Factory.createdCollections(collectionAddress)).to.be
+          .true;
+
+        const collection = await ethers.getContractAt(
+          "ERC1155Asset",
+          collectionAddress
+        );
+
+        expect(await collection.name()).to.equal(name);
+        expect(await collection.symbol()).to.equal(symbol);
+        expect(await collection.owner()).to.equal(addr1.address);
+        expect(await collection.totalMinted()).to.equal(0);
+
+        await erc1155Factory
+          .connect(addr1)
+          .createItem(collectionAddress, token);
+
+        expect(await collection.exists(token.tokenId)).to.equal(true);
+        expect(await collection.minterOf(token.tokenId)).to.equal(
+          addr1.address
+        );
+        expect(await collection.uri(token.tokenId)).to.equal(token.metadataUri);
+      }
     });
   });
 });
