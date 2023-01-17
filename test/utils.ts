2db1a4d4a1f85a7c591c52e75a3e9054557707a1
ahsenriaz <ahsenriaz2001@gmail.com>
2023-01-17 23:14:46 +0500

    Update utils.ts

--- test/utils.ts
+++ test/utils.ts
@@ -5,3 +5,34 @@ export const LISTING_TYPES = {
   FIXED_PRICE: 1,
   AUCTION: 2,
 };
+
+export type Token = {
+  to: string;
+  tokenId: number;
+  amount: number;
+  metadataUri: string;
+};
+
+export type Tokens = {
+  to: string;
+  tokenIds: Array<number>;
+  amounts: Array<number>;
+  metadataUris: Array<string>;
+};
+
+export enum ListingType {
+  NONE,
+  FIXED_PRICE,
+  AUCTION,
+}
+
+export type List = {
+  listingType: ListingType;
+  nftContract: string;
+  listedQuantity: string;
+  tokenId: string;
+  price: string;
+  startTime: string;
+  endTime: string;
+};
+
