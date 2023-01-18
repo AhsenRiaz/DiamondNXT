68f9212107684967adad034e392fd166b2213965
ahsenriaz <ahsenriaz2001@gmail.com>
2023-01-19 00:12:08 +0500

    Update Marketplace.test.ts

--- test/Marketplace.test.ts
+++ test/Marketplace.test.ts
@@ -44,7 +44,7 @@ describe("Marketplace Contract", () => {
     it("should expect the deployment address of marketplace contract", async () => {
       assert(marketplace.address);
     });
-    it("should expect the deployment address of marketplace contract", async () => {
+    it("should expect the deployment address of erc1155 asset contract", async () => {
       assert(erc1155Asset.address);
     });
   });
@@ -193,87 +193,87 @@ describe("Marketplace Contract", () => {
     });
   });
 
-  // it("should batch list tokens", async () => {
-  //   let batchTokens = [
-  //     {
-  //       to: owner.address,
-  //       tokenId: 5,
-  //       amount: 1,
-  //       metadataUri: `ipfs://token-5`,
-  //     },
-  //     {
-  //       to: owner.address,
-  //       tokenId: 6,
-  //       amount: 1,
-  //       metadataUri: `ipfs://token-6`,
-  //     },
-  //     {
-  //       to: owner.address,
-  //       tokenId: 8,
-  //       amount: 5,
-  //       metadataUri: `ipfs://token-8`,
-  //     },
-  //   ];
+  it("should batch list tokens", async () => {
+    let batchTokens = [
+      {
+        to: owner.address,
+        tokenId: 5,
+        amount: 1,
+        metadataUri: `ipfs://token-5`,
+      },
+      {
+        to: owner.address,
+        tokenId: 6,
+        amount: 1,
+        metadataUri: `ipfs://token-6`,
+      },
+      {
+        to: owner.address,
+        tokenId: 8,
+        amount: 5,
+        metadataUri: `ipfs://token-8`,
+      },
+    ];
 
-  //   let payloads: Marketplace.ListDataStruct[] = [
-  //     {
-  //       tokenId: "5",
-  //       nftContract: erc1155Asset.address,
-  //       listedQuantity: "1",
-  //       listingType: ListingType.FIXED_PRICE,
-  //       startTime: "0",
-  //       endTime: (new Date("2077-12-10").getTime() / 1000).toFixed(0),
-  //       price: "2000000000000000000",
-  //     },
-  //     {
-  //       tokenId: "6",
-  //       nftContract: erc1155Asset.address,
-  //       listedQuantity: "1",
-  //       listingType: ListingType.FIXED_PRICE,
-  //       startTime: "0",
-  //       endTime: (new Date("2077-12-10").getTime() / 1000).toFixed(0),
-  //       price: "2000000000000000000",
-  //     },
-  //     {
-  //       tokenId: "8",
-  //       nftContract: erc1155Asset.address,
-  //       listedQuantity: "5",
-  //       listingType: ListingType.FIXED_PRICE,
-  //       startTime: "0",
-  //       endTime: (new Date("2077-12-10").getTime() / 1000).toFixed(0),
-  //       price: "2000000000000000000",
-  //     },
-  //   ];
+    let payloads: Marketplace.ListDataStruct[] = [
+      {
+        tokenId: "5",
+        nftContract: erc1155Asset.address,
+        listedQuantity: "1",
+        listingType: ListingType.FIXED_PRICE,
+        startTime: "0",
+        endTime: (new Date("2077-12-10").getTime() / 1000).toFixed(0),
+        price: "2000000000000000000",
+      },
+      {
+        tokenId: "6",
+        nftContract: erc1155Asset.address,
+        listedQuantity: "1",
+        listingType: ListingType.FIXED_PRICE,
+        startTime: "0",
+        endTime: (new Date("2077-12-10").getTime() / 1000).toFixed(0),
+        price: "2000000000000000000",
+      },
+      {
+        tokenId: "8",
+        nftContract: erc1155Asset.address,
+        listedQuantity: "5",
+        listingType: ListingType.FIXED_PRICE,
+        startTime: "0",
+        endTime: (new Date("2077-12-10").getTime() / 1000).toFixed(0),
+        price: "2000000000000000000",
+      },
+    ];
 
-  //   for (var i = 0; i < batchTokens.length; i++) {
-  //     await erc1155Asset
-  //       .connect(owner)
-  //       .mint(
-  //         batchTokens[i].to,
-  //         batchTokens[i].tokenId,
-  //         batchTokens[i].amount,
-  //         batchTokens[i].metadataUri
-  //       );
-  //   }
-  //   await marketplace.connect(owner).listBatch(payloads);
+    for (var i = 0; i < batchTokens.length; i++) {
+      await erc1155Asset
+        .connect(owner)
+        .mint(
+          batchTokens[i].to,
+          batchTokens[i].tokenId,
+          batchTokens[i].amount,
+          batchTokens[i].metadataUri
+        );
+    }
+    await marketplace.connect(owner).listBatch(payloads);
 
-  //   for (var i = 0; i < payloads.length; i++) {
-  //     listedTokenDetails = await marketplace.getListingDetails(
-  //       payloads[i].nftContract,
-  //       owner.address,
-  //       payloads[i].tokenId
-  //     );
+    for (var i = 0; i < payloads.length; i++) {
+      listedTokenDetails = await marketplace.getListingDetails(
+        payloads[i].nftContract,
+        owner.address,
+        payloads[i].tokenId
+      );
 
-  //     expect(listedTokenDetails.initialized).to.be.true;
-  //     expect(listedTokenDetails.nftContract).to.equal(payloads[i].nftContract);
-  //     expect(listedTokenDetails.owner).to.equal(owner.address);
-  //     expect(listedTokenDetails.tokenId).to.equal(payloads[i].tokenId);
-  //     expect(listedTokenDetails.listingType).to.equal(payloads[i].listingType);
-  //     expect(listedTokenDetails.listedQuantity).to.equal(
-  //       payloads[i].listedQuantity
-  //     );
-  //     expect(listedTokenDetails.price).to.equal(payloads[i].price);
-  //     expect(listedTokenDetails.endTime).to.equal(payloads[i].endTime);
-  //   }
-  // });
+      expect(listedTokenDetails.initialized).to.be.true;
+      expect(listedTokenDetails.nftContract).to.equal(payloads[i].nftContract);
+      expect(listedTokenDetails.owner).to.equal(owner.address);
+      expect(listedTokenDetails.tokenId).to.equal(payloads[i].tokenId);
+      expect(listedTokenDetails.listingType).to.equal(payloads[i].listingType);
+      expect(listedTokenDetails.listedQuantity).to.equal(
+        payloads[i].listedQuantity
+      );
+      expect(listedTokenDetails.price).to.equal(payloads[i].price);
+      expect(listedTokenDetails.endTime).to.equal(payloads[i].endTime);
+    }
+  });
 });
